1dad73ef2921209146059c8e0d2b3940b02f6e69
Cidora <cidoraradaelli@gmail.com>
2022-05-13 22:53:46 -0300

    Encontro Remoto 2 - Atividade

--- numero_alunos.js
+++ numero_alunos.js
@@ -1,13 +1,22 @@
+// Considere a quantidade de alunos presentes na sala, percorra do zero até o número total de alunos e retornar os seguintes resultados:
 
-for ( var aluno = 0; aluno <=33; aluno++ ) {
+// Se o número for par, escreva 'par' e o número correspondente.
 
-    if( aluno == 0){
-        console.log(" Número zero " + aluno );
+// Se o número for ímpar, escreva 'ímpar' e o número correspondente.
+
+// Se o número for zero, escreva 'zero' e o número correspondente.
+
+
+
+for ( var numAlunos = 0; numAlunos <=33; numAlunos++ ) {
+
+    if( numAlunos == 0){
+        console.log(" Número zero " + numAlunos );
     } else {
-        if( aluno % 2 == 0){
-            console.log( " Este numero é par - " + aluno)
+        if( numAlunos % 2 == 0){
+            console.log( " Este numero é par - " + numAlunos);
         } else{
-            console.log( " Este numero é impar - " + aluno)
+            console.log( " Este numero é impar - " + numAlunos);
         }
     }
 }
