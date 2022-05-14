321fd61d01ab573c66fc629b3e82c40a0b7c28ab
Cidora <cidoraradaelli@gmail.com>
2022-05-13 22:47:09 -0300

    Encontro Remoto 2 - Atividade

---
+++ numero_alunos.js
@@ -0,0 +1,13 @@
+
+for ( var aluno = 0; aluno <=33; aluno++ ) {
+
+    if( aluno == 0){
+        console.log(" Número zero " + aluno );
+    } else {
+        if( aluno % 2 == 0){
+            console.log( " Este numero é par - " + aluno)
+        } else{
+            console.log( " Este numero é impar - " + aluno)
+        }
+    }
+}
